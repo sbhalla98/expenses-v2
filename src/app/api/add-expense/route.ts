894f20b25c9cc4c5ae61f6dff369029634dc6534
sbhalla98 <sonalibhalla98@gmail.com>
2025-01-20 18:43:52 +0530

    Added mongodb

--- src/app/api/add-expense/route.ts
+++ src/app/api/add-expense/route.ts
@@ -1,7 +1,20 @@
+import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request): Promise<NextResponse> {
-  const data = await request.json();
-  console.log("request", request, data);
-  return NextResponse.json({ message: "Hello, World!" });
+  try {
+    const body = await request.json();
+    const client = await clientPromise;
+
+    const db = client.db("expenses-v2");
+    const collection = db.collection("expenses");
+
+    const result = await collection.insertOne(body);
+    return NextResponse.json(result);
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Error adding expense" },
+      { status: 500 }
+    );
+  }
 }
