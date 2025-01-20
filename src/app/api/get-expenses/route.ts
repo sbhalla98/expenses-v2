8b5e61b4e901b774ac9a50384f9d73c6796cdb4b
sbhalla98 <sonalibhalla98@gmail.com>
2025-01-20 18:55:03 +0530

    added new api

--- src/app/api/get-expenses/route.ts
+++ src/app/api/get-expenses/route.ts
@@ -1,18 +1,16 @@
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
 
-export async function POST(request: Request): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
   try {
-    const id = uuidv4();
-    const body = await request.json();
     const client = await clientPromise;
 
     const db = client.db("expenses-v2");
     const collection = db.collection("expenses");
 
-    const result = await collection.insertOne({ ...body, id });
-    return NextResponse.json(result);
+    const data = await collection.find({}).toArray();
+
+    return NextResponse.json({ success: true, data });
   } catch (e) {
     return NextResponse.json(
       { message: "Error adding expense" },
--- src/app/my-expenses/page.tsx
+++ src/app/my-expenses/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
 import GroupedExpenseList from "@/components/common/grouped-expense-list";
+import { useToast } from "@/hooks/use-toast";
 import { sample } from "@/lib/constants";
 import { getGroupedByDate } from "@/lib/utils";
+import { useEffect } from "react";
 
 export default function MyExpenses() {
+  const { toast } = useToast();
   const groupedData = getGroupedByDate(sample);
+
+  const fetchExpenses = async () => {
+    try {
+      const response = await fetch("/api/get-expenses");
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      toast({
+        title: "An error occurred",
+        description: "Failed to fetch expenses",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
   return (
     <div>
       <h1 className="text-2xl font-semibold text-gray-800 p-6">My Expenses</h1>
