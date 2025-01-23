019eb3217ecd06482500b6ce50d06e7936dfdd75
sbhalla98 <sonalibhalla98@gmail.com>
2025-01-23 18:26:03 +0530

    added mirgate -data

---
+++ src/app/api/migrate-data/route.ts
@@ -0,0 +1,27 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export async function POST(request: Request): Promise<NextResponse> {
+  try {
+    const body = await request.json();
+    const client = await clientPromise;
+
+    const db = client.db("expenses-v2");
+    const collection = db.collection("expenses2");
+    const id = uuidv4();
+    const userId = request.headers.get("user-id") || "9876";
+
+    const reqBody = body.map((item: any) => {
+      return { ...item, id: uuidv4(), userId };
+    });
+
+    const data = await collection.insertMany(reqBody);
+    return NextResponse.json({ success: true, data });
+  } catch (e) {
+    return NextResponse.json(
+      { success: false, message: "Error adding expense", details: e },
+      { status: 500 },
+    );
+  }
+}
