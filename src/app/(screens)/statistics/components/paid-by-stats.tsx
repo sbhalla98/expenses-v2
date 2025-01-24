549833a654738b548054b8ad3604c7e1200b3611
sbhalla98 <sonalibhalla98@gmail.com>
2025-01-25 01:48:11 +0530

    label fix

--- src/app/(screens)/statistics/components/paid-by-stats.tsx
+++ src/app/(screens)/statistics/components/paid-by-stats.tsx
@@ -1,18 +1,31 @@
 "use client";
 
 import GroupedExpenseList from "@/components/common/grouped-expense-list";
+import { PERSONS } from "@/lib/constants";
 import { getExpenseAmount, getGroupedByKey } from "@/lib/utils";
-import { Expense } from "@/store/use-config-store";
+import useConfigStore, { Expense } from "@/store/use-config-store";
 
 type PaidByStatsProps = {
   expenses: Expense[];
 };
 
 export default function PaidByStats({ expenses }: PaidByStatsProps) {
+  const configStore = useConfigStore();
+
+  const getTitle = (title: string) => {
+    if (title === PERSONS.PERSON1 || title === PERSONS.PERSON2) {
+      return configStore[title];
+    }
+    return title;
+  };
+
   const groupedData = getGroupedByKey(expenses, "paidBy");
-  const sortedGroupedData = groupedData.sort(
-    (a, b) => getExpenseAmount(b.data) - getExpenseAmount(a.data),
-  );
+  const sortedGroupedData = groupedData
+    .sort((a, b) => getExpenseAmount(b.data) - getExpenseAmount(a.data))
+    .map((group) => ({
+      ...group,
+      title: getTitle(group.title),
+    }));
 
   return <GroupedExpenseList groupedExpenses={sortedGroupedData} />;
 }
--- src/app/(screens)/statistics/components/paid-for-stats.tsx
+++ src/app/(screens)/statistics/components/paid-for-stats.tsx
@@ -3,13 +3,22 @@
 import GroupedExpenseList from "@/components/common/grouped-expense-list";
 import { PERSONS } from "@/lib/constants";
 import { getExpenseAmount, getGroupedByKey } from "@/lib/utils";
-import { Expense } from "@/store/use-config-store";
+import useConfigStore, { Expense } from "@/store/use-config-store";
 
 type PaidForStatsProps = {
   expenses: Expense[];
 };
 
 export default function PaidForStats({ expenses }: PaidForStatsProps) {
+  const configStore = useConfigStore();
+
+  const getTitle = (title: string) => {
+    if (title === PERSONS.PERSON1 || title === PERSONS.PERSON2) {
+      return configStore[title];
+    }
+    return title;
+  };
+
   const processExpenses = () => {
     const result: Expense[] = [];
     expenses.forEach((expense) => {
@@ -38,9 +47,12 @@ export default function PaidForStats({ expenses }: PaidForStatsProps) {
   const processExpensesResult = processExpenses();
 
   const groupedData = getGroupedByKey(processExpensesResult, "paidFor");
-  const sortedGroupedData = groupedData.sort(
-    (a, b) => getExpenseAmount(b.data) - getExpenseAmount(a.data),
-  );
+  const sortedGroupedData = groupedData
+    .sort((a, b) => getExpenseAmount(b.data) - getExpenseAmount(a.data))
+    .map((group) => ({
+      ...group,
+      title: getTitle(group.title),
+    }));
 
   return <GroupedExpenseList groupedExpenses={sortedGroupedData} />;
 }
